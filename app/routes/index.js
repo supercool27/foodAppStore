4d0c10463faa1a111f5e1b0ff3509adbf53763b6
supercool27 <manojpali@live.com>
2022-06-13 16:39:41 +0530

    First commit

---
+++ app/routes/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import Products from '../pages/Products';
+import Checkout from '../pages/Checkout';
+import Receipt from '../pages/Receipt';
+
+import themes from '../styles/theme.style';
+
+// const Route = createStackNavigator(
+// {
+//   Products: { screen: Products},
+//   Checkout: { screen: Checkout},
+//   Receipt: { screen: Receipt}
+// },
+// {
+//  navigationOptions: {
+//  	headerStyle: {
+//  		backgroundColor: themes.BACKGROUND_COLOR,
+//  		paddingHorizontal: 10,
+//  	},
+//  	headerTintColor: '#fff'
+//  }
+// }
+// );
+
+
+const Route = createAppContainer(createStackNavigator(
+	{
+	  Products: { screen: Products},
+	  Checkout: { screen: Checkout},
+	  Receipt: { screen: Receipt}
+	},
+	{
+	 navigationOptions: {
+		 headerStyle: {
+			 backgroundColor: themes.BACKGROUND_COLOR,
+			 paddingHorizontal: 10,
+		 },
+		 headerTintColor: '#fff'
+	 }
+	}
+	));
+
+export default Route;
